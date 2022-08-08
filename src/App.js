1d87a2a330f6338f9eb9016045caf3dd57735ffd
Mabast <mabast.niga@yahoo.com>
2022-08-08 02:04:40 -0500

    added subroutes and nav bar styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./routes/Home/Home";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
+      {/* Home */}
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={"Shop"} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import CrwnLogo from "../assets/crown.svg";
+
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <Link to="/" className="logo-container">
+          <img src={CrwnLogo} alt="logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navbar;
