cc771e377862b745358f553a5bfec25730e43712
Mabast <mabast.niga@yahoo.com>
2022-08-13 23:51:38 -0500

    created on auth state change handler for firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,6 +8,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 
 //firestore setup
@@ -89,3 +90,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 //signout a user with email and password
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
