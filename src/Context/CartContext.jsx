aab8a8d70d50802149077d47055887fd038b51d2
Mabast <mabast.niga@yahoo.com>
2022-08-16 23:01:39 -0500

    created functionality to add items to cart

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -2,11 +2,36 @@ import React, { useState, createContext } from "react";
 
 export const CartContext = createContext({
   isClicked: false,
+  setIsClicked: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
+
+export const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartProvider = ({ children }) => {
   const [isClicked, setIsClicked] = useState(CartContext);
-  const value = { isClicked, setIsClicked };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isClicked, setIsClicked, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
