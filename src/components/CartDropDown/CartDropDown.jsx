8c41a8d3b1c0c37773dd39a55b3d50e8f81e35c2
Mabast <mabast.niga@yahoo.com>
2022-08-16 23:03:58 -0500

    used cart item component to render items added to cart

--- src/components/CartDropDown/CartDropDown.jsx
+++ src/components/CartDropDown/CartDropDown.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./CartDropDown.scss";
 import Button from "../Button/Button";
+import CartItem from "../CartItem/CartItem";
+import { CartContext } from "../../Context/CartContext";
 
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item._id} cartItem={item} />
+        ))}
+      </div>
+
       <Button>Checkout</Button>
     </div>
   );
