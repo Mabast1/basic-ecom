8466020c516e54c790b3db25241871880c3c982f
Mabast <mabast.niga@yahoo.com>
2022-08-21 23:24:39 -0500

    Create CategoriesContext.jsx

---
+++ src/Context/CategoriesContext.jsx
@@ -0,0 +1,36 @@
+import React, { useState, createContext, useEffect } from "react";
+
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "../utils/firebase/firebase.utils.js";
+import productsData from "../data.js";
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+  const value = { categoriesMap };
+
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", productsData);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocuments();
+      setCategoriesMap(categoriesMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
