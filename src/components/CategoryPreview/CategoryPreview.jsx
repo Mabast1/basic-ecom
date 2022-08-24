901e0c2c6b4bce8ee34746f0922d64d20de15480
Mabast <mabast.niga@yahoo.com>
2022-08-23 23:41:40 -0500

    added link to route title

--- src/components/CategoryPreview/CategoryPreview.jsx
+++ src/components/CategoryPreview/CategoryPreview.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 import ProductCard from "../ProductCard/ProductCard";
@@ -8,11 +9,13 @@ const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">
-          {title.toUpperCase()}
-          <span className="view-more">view more</span>
-          <MdOutlineArrowForwardIos size={20} className="arrow" />
-        </span>
+        <Link to={title}>
+          <span className="title">
+            {title.toUpperCase()}
+            <span className="view-more">view more</span>
+            <MdOutlineArrowForwardIos size={20} className="arrow" />
+          </span>
+        </Link>
       </h2>
       <div className="preview">
         {products.slice(0, 4).map((product) => (
