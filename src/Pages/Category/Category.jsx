dfbbe0856be4a8a9204e361677504b33672f5d25
Mabast <mabast.niga@yahoo.com>
2022-08-23 23:40:12 -0500

    created category component

---
+++ src/Pages/Category/Category.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import "./Category.scss";
+import { CategoriesContext } from "../../Context/CategoriesContext";
+import ProductCard from "../../components/ProductCard/ProductCard";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-page-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Category;
